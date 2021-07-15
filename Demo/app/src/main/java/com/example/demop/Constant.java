a1f443509ede7e9d903c176038fd11f3c8b4c2d2
tyrionchen <tyrionchen@tencent.com>
2021-07-15 14:55:04 +0800

    chore: 更新体验码

--- Demo/app/src/main/java/com/example/demop/Constant.java
+++ Demo/app/src/main/java/com/example/demop/Constant.java
@@ -2,9 +2,9 @@ package com.example.demop;
 
 public class Constant {
     // 云游戏团队提供的端游体验码, 有效期7天
-    public static final String PC_EXPIRATION_CODE = "3LEK8E9N";
+    public static final String PC_EXPIRATION_CODE = "LP1NVWNL";
     // 云游戏团队提供的手游体验码, 有效期7天
-    public static final String MOBILE_EXPIRATION_CODE = "FJTBCSC1";
+    public static final String MOBILE_EXPIRATION_CODE = "L2DTCR5U";
 
     // 应用的AppID
     public static final long APP_ID = 123456789;
