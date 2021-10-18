65478ad8d2767f8799308787821c42223319adb4
tyrionchen(陈颜胤) <tyrionchen@tencent.com>
2021-10-18 20:12:57 +0800

    更新体验码

--- Demo/app/src/main/java/com/example/demop/Constant.java
+++ Demo/app/src/main/java/com/example/demop/Constant.java
@@ -2,9 +2,9 @@ package com.example.demop;
 
 public class Constant {
     // 云游戏团队提供的端游体验码, 有效期7天
-    public static final String PC_GAME_CODE = "4Z2J7MDP";
+    public static final String PC_GAME_CODE = "UKUMB812";
     // 云游戏团队提供的手游体验码, 有效期7天
-    public static final String MOBILE_GAME_CODE = "19DZAL9I";
+    public static final String MOBILE_GAME_CODE = "73GALV36";
     // 应用的AppID
     public static final long APP_ID = 123456789;
     // 日志标签
