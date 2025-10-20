096940f3a020ba62d34caf43ef60c70032b11268
4570235 <4570235@QQ.COM>
2025-10-20 20:27:18 +0800

    更新 TcrSdk 到 3.33.0

--- TcrSdk/Demo/TcrDemo/app/src/tcrdemo/java/com/tencent/tcrdemo/gameplay/ExperiencePageActivity.java
+++ TcrSdk/Demo/TcrDemo/app/src/tcrdemo/java/com/tencent/tcrdemo/gameplay/ExperiencePageActivity.java
@@ -64,7 +64,7 @@ public class ExperiencePageActivity extends AppCompatActivity implements Experie
     private void setupDefaultId() {
         String experienceCode = mPrefs.getString(getResources().getString(R.string.key_experience_code), "");
         if (experienceCode.isEmpty()) {
-            experienceCode = "NWPY2MWL";
+            experienceCode = "5N02FBYG";
         }
         String userID = mPrefs.getString(getResources().getString(R.string.key_set_userid), "");
         String hostUserId = mPrefs.getString(getResources().getString(R.string.key_set_host_userid), "");
